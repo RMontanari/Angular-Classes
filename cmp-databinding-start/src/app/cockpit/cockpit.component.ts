3ece7c8b349f5e0b630353ff49b33b378b012fd0
RMontanari <rmontanari@f3capital.com.br>
2024-02-09 15:54:09 -0300

    add(): cmp-databinding-start

---
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,37 @@
+import { Component, ElementRef, Output, ViewChild } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { ServerElementComponent } from '../server-element/server-element.component';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrl: './cockpit.component.css'
+})
+export class CockpitComponent {
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  
+  // newServerName = '';
+  // newServerContent = '';
+
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  onAddServer(nameInput: HTMLInputElement)  {
+    this.serverCreated.emit({
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
+  });
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value, 
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+
+  }
+}
---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.css',
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class ServerElementComponent implements OnInit {
+  @Input('srvelement') element: { type: string, name: string, content: string };
+
+  ngOnInit() {
+  }
+}
